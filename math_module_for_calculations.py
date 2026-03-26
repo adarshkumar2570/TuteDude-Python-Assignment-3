2d4af4ad65dfd7ef4f1faff607bdef55287f96de
Adarsh <adarshkumar2570@gmail.com>
2026-03-27 00:43:30 +0530

    Add files via upload

---
+++ math_module_for_calculations.py
@@ -0,0 +1,13 @@
+import math
+
+from math import sqrt
+n = int(input("Enter a number: "))
+print(f"square root: {sqrt(n)}")
+
+from math import log
+print(f"Logarithm: {log(n)}")
+
+from math import sin
+print(f"Sine: {sin(n)}")
+
+
